beeae3e9f737b97d18a3d4c1b8a1dfa3603ec6f8
Pratik Patil <Pratikpatil1311@gmail.com>
2024-08-12 16:53:51 +0530

    Portfolio_site

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Resume from './components/Resume/Resume';
+import Projects from './components/Projects/Projects';
+import Footer from './components/Footer/Footer';
+import Cursor from './components/cursor/Cursor';
+import Contact from './components/client_contact/client_contact'; // Correct component import
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import Loader from './components/Loader/Loader'
+
+const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate a delay for loading data or content
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Adjust the delay as needed
+
+    return () => clearTimeout(timer);
+  }, []);
+
+ 
+   
+  return (
+   
+    <Router>
+      <div>
+      <div>
+    {loading ? <Loader /> : <div></div>}
+  </div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<>
+            <Home />
+            <Resume />
+            <Projects />
+            <Footer />
+            <Cursor />
+          </>} />
+          <Route path="/contact" element={<>
+            <Contact />
+          </>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
